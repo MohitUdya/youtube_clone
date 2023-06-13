72fdf54aca88b1064d731a28e1971d26307868ff
MohitUdya <108533936+MohitUdya@users.noreply.github.com>
2023-06-13 17:04:24 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import reactDOm from 'react-dom/client'
+import App from './App'
+import './index.css'
+const root = reactDOm.createRoot(document.getElementById('root'))
+
+root.render(<App/>)
